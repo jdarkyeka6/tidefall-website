e7e3350f7b7d0be2733c5c649d8449b966632ee0
jake <jakedryka1@gmail.com>
2026-08-12 19:38:44 +0800

    Add Tidefall Supabase client

---
+++ supabase-client.js
@@ -0,0 +1,3 @@
+const TIDEFALL_SUPABASE_URL = 'https://yrbjpwtgijgcvsoqvkrr.supabase.co';
+const TIDEFALL_SUPABASE_KEY = 'sb_publishable_uRVq3HdssKGyYbKheuA2VA_o19IoIjg';
+window.tidefallSupabase = window.supabase.createClient(TIDEFALL_SUPABASE_URL, TIDEFALL_SUPABASE_KEY);
